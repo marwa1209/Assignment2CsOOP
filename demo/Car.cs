89f86b5ee5bec55a907151462312418f17960101
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-11 21:10:31 +0300

    class vs struct

---
+++ demo/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class Car
+    {
+        public Car(int id, string model, double speed)
+        {
+            Id = id;
+            Model = model;
+            Speed = speed;
+        }
+        public Car(int id, string model)
+        {
+            Id = id;
+            Model = model;
+        }
+        public Car(int id)
+        {
+            Id = id;
+        }
+        public Car()
+        {
+        }
+        #region properties
+        public int Id { get; set; }
+        public string Model { get; set; }
+
+        public double Speed { get; set; }
+        #endregion
+        public override string ToString()
+        {
+             return $"Id = {Id}\nModel={Model}\nSpeed = {Speed}";
+
+        }
+    }
+}
